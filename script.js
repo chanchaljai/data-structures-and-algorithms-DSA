21a5ba532417a994856bbe612a1be431dce07301
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-16 09:56:32 +0530

    math

--- script.js
+++ script.js
@@ -2,7 +2,14 @@ let a = 8;
 let b = 3;
 
 
-console.log(a%b);
+console.log(a+b);  // 11
+console.log(a-b);  // 5
+console.log(a*b);  // 24
+console.log(a/b);  // 2.6666666666666665
+console.log(Math.floor(a/b));  // 2  use this method for integer value
+console.log(a%b); // 2, 2 is reminder
 
-console.log(b%a);
+console.log(b%a);   // 3, 3 me 8 se bhag nhi lagega toh 3 waise hi rah jayega
 
+console.log(b/a);  // 0.375
+console.log(Math.floor(b/a));  // 0
