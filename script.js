ba6c67af2ce7f96f0f4d36dc20a3ea6011e1b82b
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-17 20:12:06 +0530

    sum of digits in any number

--- script.js
+++ script.js
@@ -1,15 +1,10 @@
-let a = 8;
-let b = 3;
+let a = 4567;
+// find add of all digita. 4+5+6+7 = 20
 
-
-console.log(a+b);  // 11
-console.log(a-b);  // 5
-console.log(a*b);  // 24
-console.log(a/b);  // 2.6666666666666665
-console.log(Math.floor(a/b));  // 2  use this method for integer value
-console.log(a%b); // 2, 2 is reminder
-
-console.log(b%a);   // 3, 3 me 8 se bhag nhi lagega toh 3 waise hi rah jayega
-
-console.log(b/a);  // 0.375
-console.log(Math.floor(b/a));  // 0
+let sum = 0;
+while(a>0){
+    let rem = a%10;
+    sum = sum + rem;
+    a = Math.floor(a/10);
+}
+console.log(sum);
