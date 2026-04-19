c7853f73fae1ec9b62438616c18bc78f38a0928b
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-19 07:26:09 +0530

    relational operation

--- script.js
+++ script.js
@@ -1,10 +1,7 @@
-let a = 4567;
-// find add of all digita. 4+5+6+7 = 20
+// > < >= <= !=
 
-let sum = 0;
-while(a>0){
-    let rem = a%10;
-    sum = sum + rem;
-    a = Math.floor(a/10);
-}
-console.log(sum);
+console.log(10>5);  //true
+console.log(10<5);  //false
+console.log(10>=5); //true
+console.log(10<=5); //false
+console.log(10!=5); //true
