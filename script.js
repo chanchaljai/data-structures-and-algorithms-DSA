b6f2e151c47c423e472a7516814cb8747175f341
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-19 15:59:29 +0530

    math operation

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-console.log(Math.round(3.6)); // 4
-console.log(Math.round(3.4)); // 3
-console.log(Math.floor(3.5)); // 3
-console.log(Math.ceil(3.5)); // 4
+console.log(Math.trunc(10.98));  // 10 Remove the decimal
+console.log(Math.pow(2, 3));    // 2^3 = 8  2 to the power of 3
+console.log(Math.sqrt(9));       // 3   square root
+console.log(Math.cbrt(64));       // 4   cubic root
+console.log(Math.abs(-10));      // 10
+console.log(Math.max(1, 2, 3));  // 3
+console.log(Math.min(1, 2, 3));  // 1  
+console.log(Math.random(   ));  // 0 to 1  ke bich deciml apne mn se kuchh bhi
+console.log(toFixed(12.98765433, 2));   // 12.98
