e311e8cf72b2b46a80c90732b8e7f62f8ee32877
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-19 16:10:34 +0530

    random otp generate

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-console.log(Math.trunc(10.98));  // 10 Remove the decimal
-console.log(Math.pow(2, 3));    // 2^3 = 8  2 to the power of 3
-console.log(Math.sqrt(9));       // 3   square root
-console.log(Math.cbrt(64));       // 4   cubic root
-console.log(Math.abs(-10));      // 10
-console.log(Math.max(1, 2, 3));  // 3
-console.log(Math.min(1, 2, 3));  // 1  
-console.log(Math.random(   ));  // 0 to 1  ke bich deciml apne mn se kuchh bhi
-console.log(toFixed(12.98765433, 2));   // 12.98
+console.log(Math.floor(Math.random()*8000) + 1000);
