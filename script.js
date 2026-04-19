364adc76d246d252b03015b907a4a864a2227543
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-19 16:54:42 +0530

    area of tringle

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-console.log(Math.floor(Math.random()*8000) + 1000);
+// area of triangle of heron's formula
+
+let a = 3;
+let b = 4;
+let c = 5;
+let s = (a + b + c) / 2;
+let area = Math.sqrt(s * (s - a) * (s - b) * (s - c));
+console.log(area);  // 6
