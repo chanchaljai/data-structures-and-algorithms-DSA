e5f4a1c7cd810effbb0abe200244402153865d49
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-20 17:55:41 +0530

    string to number

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-var ans =  prompt("What is Your Age?");
+var ans = Number(prompt("What is Your Age?")); // prompt se string milta hai
+console.log(ans);
 
-if(ans < 18)
-{
-    alert("You are not eligible to vote");
-}
-else
-{
-    alert("You are eligible to vote");
+if (ans < 18) {
+  alert("You are not eligible to vote");
+} else {
+  alert("You are eligible to vote");
 }
