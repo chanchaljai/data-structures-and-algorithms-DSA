d52541e5fc6e12cf1e47136b1b18c7e547ce83e9
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-20 18:04:24 +0530

    valid voter Check

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var ans = Number(prompt("What is Your Age?")); // prompt se string milta hai
 console.log(ans);
 
 if (ans < 18) {
-  alert("You are not eligible to vote");
+  console.log("You are not eligible to vote");
 } else {
-  alert("You are eligible to vote");
+  console.log("You are eligible to vote");
 }
