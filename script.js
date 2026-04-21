339495b6445550807c7ccb8609ebb2830f9739c7
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-21 09:54:38 +0530

    Discount on compare method

--- script.js
+++ script.js
@@ -1,45 +1,17 @@
-// shop pe Shopping below 5000 0 discount 
-// 5001 to 7000 5% discount
-// 7001 to 9000 10% discount
-// mote than 9000 20% discount
+// Shopping Discount
+let amount = Number(prompt("Enter the amount"));
+if(amount>0 && amount<5000){
+    // No Discount
+    console.log(amount);
+}else if(amount> 5000 && amount <=7000){
+    // 5% Discount
+    discount = Number(Math.floor((5*amount)/100));
+    console.log("Amount: "+amount);
+    console.log("Discount: "+discount);
+    
+    
+    bill = amount - discount;
+    console.log("Final Bill: "+bill);
+    
+}
 
-let price = Number(prompt("Enter your price"));
-// shop pe Shopping below 5000 0 discount 
-
-if(price <= 5000){
-    // No discount then 
-     TotalBill = price;
-     console.log("Your Shopping Amount is",price);
-     console.log("Your Discount is",0);
-     console.log("Your total bill is",TotalBill);
-     }
-// 5001 to 7000 5% discount
-
-if(price > 5000 && price <= 7000){
-    // 5% discount then 
-    discount = (price * 5)/100;
-    TotalBill = price - discount;
-    console.log("Your Shopping Amount is",price);
-    console.log("Your Discount is",discount);
-    console.log("Your total bill is",TotalBill);
-    }
-// 7001 to 9000 10% discount
-
-if(price > 7000 && price <= 9000){
-    // 10% discount then 
-    discount = (price * 10)/100;
-    TotalBill = price - discount;
-    console.log("Your Shopping Amount is",price);
-    console.log("Your Discount is",discount);
-    console.log("Your total bill is",TotalBill);
-    }
-// mote than 9000 20% discount
-
-if(price > 9000){
-    // 20% discount then 
-    discount = (price * 20)/100;
-    TotalBill = price - discount;
-    console.log("Your Shopping Amount is",price);
-    console.log("Your Discount is",discount);
-    console.log("Your total bill is",TotalBill);
-    }
