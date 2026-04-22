c2a3a3df9ecdf93cde7e85bde4c8ac2eca5d34f7
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-22 07:15:28 +0530

    shopping

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 // Shopping Discount
 let amount = Number(prompt("Enter the amount"));
-if(amount>0 && amount<5000){
-    // No Discount
-    console.log(amount);
-}else if(amount> 5000 && amount <=7000){
-    // 5% Discount
-    discount = Number(Math.floor((5*amount)/100));
-    console.log("Amount: "+amount);
-    console.log("Discount: "+discount);
-    
-    
-    bill = amount - discount;
-    console.log("Final Bill: "+bill);
-    
-}
+if (amount > 0 && amount < 5000) {
+  // No Discount
+  console.log(amount);
+} else if (amount > 5000 && amount <= 7000) {
+  // 5% Discount
+  discount = Number(Math.floor((5 * amount) / 100));
+  console.log("Amount: " + amount);
+  console.log("Discount: " + discount);
 
+  bill = amount - discount;
+  console.log("Final Bill: " + bill);
+} else if (amount > 7000 && amount <= 9000) {
+  // 10% Discount
+  discount = Number(Math.floor((10 * amount) / 100));
+  console.log("Amount: " + amount);
+  console.log("Discount: " + discount);
+  bill = amount - discount;
+  console.log("Final Bill: " + bill);
+} else {
+  // 20% Discount
+  discount = Number(Math.floor((20 * amount) / 100));
+  console.log("Amount: " + amount);
+  console.log("Discount: " + discount);
+  bill = amount - discount;
+  console.log("Final Bill: " + bill);
+}
