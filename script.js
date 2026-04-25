bf09d112e8b39d09358ac9904a46af951f6e2cdb
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-25 12:15:00 +0530

    no. of Notes

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-// Electricity bill calculation
-//up to 100 4.2 per unit  101-200 6 per unit 201-400 8 per unit and above 13 per unit
+// find note
+// 4823  rupya me kitne ka kitna note milega
+let amount = prompt("Enter the amount");
+// No. of 500 note = 
 
-let unit = prompt("Enter the unit");
-let bill = 0;
-if (unit <= 100) {
-    bill = unit * 4.2;
-}
-else if (unit <= 200) {
-    bill = (100 * 4.2) + ((unit - 100) * 6);
-}
-else if (unit <= 400) {
-    bill = (100 * 4.2) + (100 * 6) + ((unit - 200) * 8);
-}
-else {
-    bill = (100 * 4.2) + (100 * 6) + (200 * 8) + ((unit - 400) * 13);
-}
-alert("Your bill is " + bill);
+console.log("No. of 500 note = "+Math.floor(amount/500));    // no. of 500 note
+let RestAmount = (amount%500); 
 
+// No. of 200 note =
+
+console.log("No. of 200 note = "+Math.floor(RestAmount/200));    // no. of 200 note
+let RestAmount1 = (RestAmount%200);
+
+// No. of 100 note =
+console.log("No. of 100 note = "+Math.floor(RestAmount1/100));    // no. of 100 note
+let RestAmount2 = (RestAmount1%100);
+
+// No. of 50 note =
+console.log("No. of 50 note = "+Math.floor(RestAmount2/50));    // no. of 50 note
+let RestAmount3 = (RestAmount2%50);
+
+// No. of 20 note =
+console.log("No. of 20 note = "+Math.floor(RestAmount3/20));    // no. of 20 note
+let RestAmount4 = (RestAmount3%20);
+
+// No. of 10 note =
+console.log("No. of 10 note = "+Math.floor(RestAmount4/10));    // no. of 10 note
+let RestAmount5 = (RestAmount4%10);
+
+// No. of 5 note =
+console.log("No. of 5 note = "+Math.floor(RestAmount5/5));    // no. of 5 note
+let RestAmount6 = (RestAmount5%5);
+
+// No. of 1 note =
+console.log("No. of 1 note = "+Math.floor(RestAmount6/1));    // no. of 1 note
