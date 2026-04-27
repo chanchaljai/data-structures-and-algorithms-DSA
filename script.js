a30a1907ee930f156656e4e1af2b8523dc4ed83c
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-27 06:53:11 +0530

    factor loop run up to n/2 and print n

--- script.js
+++ script.js
@@ -4,9 +4,10 @@ let n = +prompt("Enter a number");
 if (n < 0 || isNaN(n)) {
   alert("Enter a valid number");
 } else {
-  for (let i = 1; i <= n; i++) {
+  for (let i = 1; i <= Math.floor(n / 2); i++) {
     if (n % i == 0) {
       console.log(i);
     }
   }
+  console.log(n);
 }
