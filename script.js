f16c2a53d7b1ac78400deb0075e41199ea6e4cc1
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-28 07:17:23 +0530

    break and continue

--- script.js
+++ script.js
@@ -1,16 +1,8 @@
-// check prime number
+// break and continue
 
-let n = +prompt("Enter a number: ");
-let prime = true;
-console.log(isPrime(n));
-
-function isPrime(n) {
-  if (n <= 1) return false;
-  if (n <= 2) return true;
-  if (n % 2 == 0) return false;
-  // koi bhi number 2 se apne sqrt tk divide nhi hoga to aage bhi devide nhi hoga
-  for (let i = 3; i <= Math.floor(Math.sqrt(n)); i += 2) {
-    if (n % i == 0) return false;
+for (let i = 0; i < 23; i++) {
+  if (i === 11) {
+    break; // stop the loop but iske jagah continue hota to 11 skip ho jata aur 12 se print start hota aur 22 tk chalta
   }
-  return true;
+  console.log(i);
 }
