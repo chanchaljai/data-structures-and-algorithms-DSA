d41e30fe45271fed7c581273b16b17888911d583
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-04-28 07:22:43 +0530

    while loop

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// break and continue
+// while loop 
 
-for (let i = 0; i < 23; i++) {
-  if (i === 11) {
-    break; // stop the loop but iske jagah continue hota to 11 skip ho jata aur 12 se print start hota aur 22 tk chalta
-  }
-  console.log(i);
+for(start; end; change){ 
+  // code
+}
+
+start;
+while(end){
+  // code
+  change;
 }
