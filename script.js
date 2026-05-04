a453988d00a52c4b577bc558f35990fab8daf284
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-04 07:50:23 +0530

    strong number

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // strong number
 // 145  me 1! + 4! + 5! = 145 strong number kisi number ke  sabhi digit ka factorial nikal ke  add krenge number ke equal hoga to strong number hoga.
 let n = +prompt("Enter a number");
-if  (isNaN(n)) {
+if (isNaN(n) || n < 0) {
   alert("Enter a valid number");
-  console.log("Enter a valid number,","NaN is not a number");
+} else {
+  let sum = 0;
+  let num = n;
+  while (num > 0) {
+    let rem = num % 10;
+    let fact = 1;
+    for (let i = 1; i <= rem; i++) {
+      fact = fact * i;
+    }
+    sum = sum + fact;
+    num = Math.floor(num / 10);
+  }
+  if (sum == n) {
+    console.log(`${n} is a strong number`);
+  } else {
+    console.log(`${n} is not a strong number`);
+  }
 }
