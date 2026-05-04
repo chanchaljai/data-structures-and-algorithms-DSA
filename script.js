7dc2f3a8ba7c72ad2d890f4c4b4921c52821c232
Chanchal Jaiswal <chanchal123jaiswal@gmail.com>
2026-05-04 07:07:49 +0530

    reverse

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-//123 - 321
+// 1234 ko 4321 me change karna hai 
+// kisi bhi number ko reverse karna ho 
 
-a = 123;
-rev = 0;
-while(a>0){
-  rem = a%10;
-  rev = rev * 10 + rem;
-  a = Math.floor(a/10);
+let num = +prompt("Enter the number");
+
+let rev =0;
+while (num > 0) {
+  let rem = num % 10;
+  rev = rev*10 + rem;
+  num = Math.floor(num/10)
 }
-
 console.log(rev);
